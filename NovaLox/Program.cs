42478cbe27593029acc1814e904fe541dedcce11
Nyhilo <silentclowd@gmail.coom>
2019-07-27 13:18:56 -0700

    First commit

---
+++ NovaLox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NovaLox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
